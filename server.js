71b1217615f1f5941b30c0d51899ab27fe5cc52b
serhat andic <serhatandic42@gmail.com>
2024-03-12 22:10:32 +0300

    add potential endpoints

--- server.js
+++ server.js
@@ -29,6 +29,30 @@ app.get('/all', async (req, res) => {
 	} catch (error) {}
 });
 
+// user routes
+app.post('/api/users/register', () => {});
+app.post('/api/users/login', () => {});
+app.get('/api/users/profile', () => {});
+app.put('/api/users/profile', () => {});
+app.delete('/api/users/:userId', () => {});
+
+// item routes
+app.post('/api/items', () => {});
+app.get('/api/items', () => {});
+app.get('/api/items/:itemId', () => {});
+app.put('/api/items/:itemId', () => {});
+app.delete('/api/items/:itemId', () => {});
+app.patch('/api/items/:itemId/deactivate', () => {});
+app.patch('/api/items/:itemId/reactivate', () => {});
+
+// admin and favorites routes
+app.delete('/api/admin/items/:itemId', () => {});
+app.post('/api/users/favorites', () => {});
+app.delete('/api/users/favorites/:itemId', () => {});
+app.get('/api/users/favorites', () => {});
+app.get('/api/users/verify/:token', () => {});
+app.get('/api/items/category/:categoryName', () => {});
+
 app.listen(3000, () => {
 	console.log('Server is running on port 3000');
 });
